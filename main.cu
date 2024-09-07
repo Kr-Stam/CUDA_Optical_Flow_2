e15bdf8f40782c2377c10a108d2d8c317f5233f4
Kr-Stam <kristijanstameski@gmail.com>
2024-09-07 02:56:30 +0200

    Cleaned up main, fixed a memory leak and made code more maintainable

--- main.cu
+++ main.cu
@@ -16,6 +16,142 @@
 
 using namespace std;
 
+void showTest(cv::Mat src, unsigned char** prev_pyramid, unsigned char** pyramid, int testing_levels, bool test_x, bool test_y, bool test_t)
+{
+		char title[] = {'L', 'e', 'v', 'e', 'l', ' ', 'x', ' ', '\0'};
+		char title1[] = {'L', 'e', 'v', 'e', 'l', ' ', 'x', ' ',' ', 'o', 'r', 'i', 'g', 'i', 'n', 'a', 'l', '\0'};
+		int w, h;
+		for (int k = 0; k < testing_levels; k++)
+		{
+				cv::Mat transfomed_scaled = cv::Mat(src.rows, src.cols, CV_8UC1);
+				cv::Mat original_scaled = cv::Mat(src.rows, src.cols, CV_8UC3);
+				w = src.cols;
+				h = src.rows;
+				w = w >> k;
+				h = h >> k;
+
+				if(test_x){
+						title[7] = 'x';
+						title1[7] = ' ';
+						unsigned char *transformed_unscaled = (unsigned char *)malloc(w * h * sizeof(unsigned char));
+						gpu::conv_3ch_1ch_tiled(pyramid[k], w, h, transformed_unscaled, Dx_3x3, 3, 3);
+						utils::cleanup_outliers(transformed_unscaled, w, h);
+						utils::upscale_1ch(transformed_unscaled, w, h, k, transfomed_scaled.data);
+						title[6] = '0' + (char)k;
+						title1[6] = '0' + (char)k;
+						cv::imshow(title, transfomed_scaled);
+						utils::upscale_3ch(pyramid[k], w, h, k, original_scaled.data);
+						cv::imshow(title1, original_scaled);
+						free(transformed_unscaled);
+				}
+				if(test_t){
+						title[7] = 't';
+						title1[7] = ' ';
+						unsigned char *transformed_unscaledt2 = (unsigned char *)malloc(w * h * sizeof(unsigned char));
+						unsigned char *transformed_unscaledt1 = (unsigned char *)malloc(w * h * sizeof(unsigned char));
+						gpu::conv_3ch_1ch_tiled(pyramid[k], w, h, transformed_unscaledt2, Dt_3x3_n, 3, 3);
+						gpu::conv_3ch_1ch_tiled(prev_pyramid[k], w, h, transformed_unscaledt1, Dt_3x3_n, 3, 3);
+						cpu::sub_arr(transformed_unscaledt2, transformed_unscaledt1, w *h, transformed_unscaledt1);
+
+						utils::cleanup_outliers(transformed_unscaledt1, w, h);
+						utils::upscale_1ch(transformed_unscaledt1, w, h, k, transfomed_scaled.data);
+						title[6] = '0' + (char)k;
+						title1[6] = '0' + (char)k;
+						cv::imshow(title, transfomed_scaled);
+						utils::upscale_3ch(pyramid[k], w, h, k, original_scaled.data);
+						cv::imshow(title1, original_scaled);
+						free(transformed_unscaledt1);
+						free(transformed_unscaledt2);
+				}
+				if(test_y){
+						title[7] = 'y';
+						title1[7] = ' ';
+						unsigned char *transformed_unscaled = (unsigned char *)malloc(w * h * sizeof(unsigned char));
+						gpu::conv_3ch_1ch_tiled(pyramid[k], w, h, transformed_unscaled, Dy_3x3, 3, 3);
+						utils::cleanup_outliers(transformed_unscaled, w, h);
+						utils::upscale_1ch(transformed_unscaled, w, h, k, transfomed_scaled.data);
+						title[6] = '0' + (char)k;
+						title1[6] = '0' + (char)k;
+						cv::imshow(title, transfomed_scaled);
+						utils::upscale_3ch(pyramid[k], w, h, k, original_scaled.data);
+						cv::imshow(title1, original_scaled);
+						free(transformed_unscaled);
+				}
+		}
+}
+	
+//TODO: smeni go ova so CUDA vektor tipovi float2 i uchar3
+template<typename T, int ch> void alloc_pyramid(T*** pyramid, int w, int h, int levels)
+{
+    *pyramid = (T**) malloc(levels * sizeof(T *));
+    for (int k = 0; k < levels; k++)
+    {
+        (*pyramid)[k] = (T*) malloc(w * h * ch * sizeof(T));
+        w = w >> 1;
+        h = h >> 1;
+    }
+}
+
+//ova realno nema nikoja prichina zoshto da ne e void*, ama realno dava type safety takada neka sedi
+template<typename T> void free_pyramid(T*** pyramid, int levels)
+{
+		for (int i = 0; i < levels; i++){
+				free((*pyramid)[i]);
+		}
+		free(*pyramid);
+}
+
+void visualizeFlowField(cv::Mat src, unsigned char** pyramid, float** flow_pyramid, int levels, int level, int arrowRes)
+{
+	int h = src.rows >> level;
+	int w = src.cols >> level;
+	int offset = w / arrowRes;
+
+	cv::Mat test(src.rows, src.cols, CV_8UC3);
+	utils::upscale_3ch(pyramid[level], w, h, level, test.data);
+	for (int i = 0; i < h; i += offset)
+	{
+		for (int j = 0; j < w; j += offset)
+		{
+				// For every point of prev
+				//int pos = i * w + j;
+				// Find cumulative flow of all previous levels
+				float u, v;
+				u = v = 0;
+				for (int k = levels - 1; k >= level; k--)
+				{
+						int scale = k - level;
+						int tmp_i = i >> scale;
+						int tmp_j = j >> scale;
+						int tmp_pos = tmp_i * (w >> scale) + tmp_j;
+						int multiplier = 1 << scale;
+						u += (double)multiplier * flow_pyramid[k][tmp_pos * 2];
+						v += (double)multiplier * flow_pyramid[k][tmp_pos * 2 + 1];
+				}
+				
+				//Ova e staveno za skaliranje na strelkite za podobra vizuelizacija
+				if(u > offset){
+						u = offset;
+				}else if(u < - offset){
+						u = - offset;
+				}
+				if(v > offset){
+						v = offset;
+				}else if(v < - offset){
+						v = - offset;
+				}
+				int tmp_ni = (int)(v + i);
+				int tmp_nj = (int)(u + j);
+
+				//Ova e za da se izvadat nekoi problematichni presmetki
+				if(tmp_ni < 0 || tmp_nj < 0) continue;
+
+				cv::arrowedLine(test, cv::Point(j, i), cv::Point(tmp_nj, tmp_ni), cv::Scalar(0, 0, 255), 1, 8, 0, 0.4);
+		}
+	}
+	cv::imshow("Optical Flow Field", test);
+
+}
 
 int main()
 {
@@ -34,7 +170,7 @@ int main()
     }
 
     int levels = 4;
-    // initialize prevPyramid
+    // initialize prev_pyramid
     camera.read(frame);
     cv::Mat src = frame;
     cv::Mat gray(src.rows, src.cols, CV_8UC3);
@@ -42,17 +178,15 @@ int main()
     gpu::grayscale_avg(src.data, gray.data, src.rows, src.cols);
 		//cpu::grayscale_avg_cpu(src.data, gray.data, src.rows, src.cols);
 
-    unsigned char **prevPyramid = (unsigned char **)malloc(levels * sizeof(unsigned char *));
-    int w = src.cols;
-    int h = src.cols;
-    for (int k = 0; k < levels; k++)
-    {
-        prevPyramid[k] = (unsigned char *)malloc(w * h * 3 * sizeof(unsigned char));
-        w = w >> 1;
-        h = h >> 1;
-    }
-		memcpy(prevPyramid[0], gray.data, gray.cols * gray.rows * sizeof(unsigned char) * 3);
-    gpu::gauss_pyramid(prevPyramid, gray.cols, gray.rows, levels, GAUS_KERNEL_3x3, 3, 3);
+		//Allocate Pyramids
+		unsigned char** prev_pyramid;
+		alloc_pyramid<unsigned char, 3>(&prev_pyramid, src.cols, src.rows, levels);
+		unsigned char** pyramid;
+		alloc_pyramid<unsigned char, 3>(&pyramid, src.cols, src.rows, levels);
+
+		//init prev_pyramid
+		memcpy(prev_pyramid[0], gray.data, gray.cols * gray.rows * sizeof(unsigned char) * 3);
+    gpu::gauss_pyramid(prev_pyramid, gray.cols, gray.rows, levels, GAUS_KERNEL_3x3, 3, 3);
 
     // camera.set(cv::CAP_PROP_FPS, 60);
     bool test = true;
@@ -61,10 +195,12 @@ int main()
     bool test_t = true;
     int testing_levels = 3;
 
-		bool prevPyramidAlloc = true;
+		int w, h;
+		float** flow_pyramid;
+		alloc_pyramid<float, 2>(&flow_pyramid, src.cols, src.rows, levels);
+
     while (true)
     {
-
         camera.read(frame);
         src = frame;
         cv::Mat dest(src.rows, src.cols, CV_8UC3);
@@ -84,170 +220,43 @@ int main()
         gpu::bilinear_filter(gray.data, gray.data, filtered.data, w, h, ww, wh, 2, 5);
 
         // Calculate optical flow
-        w = src.cols;
-        h = src.rows;
-        unsigned char **pyramid = (unsigned char **)malloc(levels * sizeof(unsigned char *));
-				pyramid[0] = filtered.data;
+
+				//init pyramid
+				//pyramid[0] = filtered.data;
+				memcpy(pyramid[0], filtered.data, filtered.cols * filtered.rows * sizeof(unsigned char) * 3);
 				//pyramid[0] = gray.data;
-        for (int k = 1; k < levels; k++)
-        {
-            pyramid[k] = (unsigned char *)malloc(w * h * 3 * sizeof(unsigned char));
-            w = w >> 1;
-            h = h >> 1;
-        }
         // cpu::gauss_pyramid(gray.data, src.cols, src.rows, levels, pyramid);
         // gpu::gauss_pyramid(gray.data, src.cols, src.rows, levels, pyramid);
         gpu::gauss_pyramid(pyramid, src.cols, src.rows, levels, GAUS_KERNEL_3x3, 3, 3);
         //cpu::gauss_pyramid(pyramid, src.cols, src.rows, levels, GAUS_KERNEL_3x3, 3, 3);
 
         // prikaz na nivoata na gauziskata piramida
-        if (test)
-        {
-            char title[] = {'L', 'e', 'v', 'e', 'l', ' ', 'x', ' ', '\0'};
-            char title1[] = {'L', 'e', 'v', 'e', 'l', ' ', 'x', ' ',' ', 'o', 'r', 'i', 'g', 'i', 'n', 'a', 'l', '\0'};
-            for (int k = 0; k < testing_levels; k++)
-            {
-                cv::Mat transfomed_scaled = cv::Mat(src.rows, src.cols, CV_8UC1);
-                cv::Mat original_scaled = cv::Mat(src.rows, src.cols, CV_8UC3);
-                w = src.cols;
-                h = src.rows;
-                w = w >> k;
-                h = h >> k;
+				if(test) showTest(src, prev_pyramid, pyramid, testing_levels, test_x, test_y, test_t);
 
-                if(test_x){
-                    title[7] = 'x';
-                    title1[7] = ' ';
-                    unsigned char *transformed_unscaled = (unsigned char *)malloc(w * h * sizeof(unsigned char));
-                    gpu::conv_3ch_1ch_tiled(pyramid[k], w, h, transformed_unscaled, Dx_3x3, 3, 3);
-                    utils::cleanup_outliers(transformed_unscaled, w, h);
-                    utils::upscale_1ch(transformed_unscaled, w, h, k, transfomed_scaled.data);
-                    title[6] = '0' + (char)k;
-                    title1[6] = '0' + (char)k;
-                    cv::imshow(title, transfomed_scaled);
-                    utils::upscale_3ch(pyramid[k], w, h, k, original_scaled.data);
-                    cv::imshow(title1, original_scaled);
-                    free(transformed_unscaled);
-                }
-                if(test_t){
-                    title[7] = 't';
-                    title1[7] = ' ';
-                    unsigned char *transformed_unscaledt2 = (unsigned char *)malloc(w * h * sizeof(unsigned char));
-                    unsigned char *transformed_unscaledt1 = (unsigned char *)malloc(w * h * sizeof(unsigned char));
-                    gpu::conv_3ch_1ch_tiled(pyramid[k], w, h, transformed_unscaledt2, Dt_3x3_n, 3, 3);
-                    gpu::conv_3ch_1ch_tiled(prevPyramid[k], w, h, transformed_unscaledt1, Dt_3x3_n, 3, 3);
-                    cpu::sub_arr(transformed_unscaledt2, transformed_unscaledt1, w *h, transformed_unscaledt1);
-
-                    utils::cleanup_outliers(transformed_unscaledt1, w, h);
-                    utils::upscale_1ch(transformed_unscaledt1, w, h, k, transfomed_scaled.data);
-                    title[6] = '0' + (char)k;
-                    title1[6] = '0' + (char)k;
-                    cv::imshow(title, transfomed_scaled);
-                    utils::upscale_3ch(pyramid[k], w, h, k, original_scaled.data);
-                    cv::imshow(title1, original_scaled);
-                    free(transformed_unscaledt1);
-                    free(transformed_unscaledt2);
-                }
-                if(test_y){
-                    title[7] = 'y';
-                    title1[7] = ' ';
-                    unsigned char *transformed_unscaled = (unsigned char *)malloc(w * h * sizeof(unsigned char));
-                    gpu::conv_3ch_1ch_tiled(pyramid[k], w, h, transformed_unscaled, Dy_3x3, 3, 3);
-                    utils::cleanup_outliers(transformed_unscaled, w, h);
-                    utils::upscale_1ch(transformed_unscaled, w, h, k, transfomed_scaled.data);
-                    title[6] = '0' + (char)k;
-                    title1[6] = '0' + (char)k;
-                    cv::imshow(title, transfomed_scaled);
-                    utils::upscale_3ch(pyramid[k], w, h, k, original_scaled.data);
-                    cv::imshow(title1, original_scaled);
-                    free(transformed_unscaled);
-                }
-            }
-        }
-        
-        float **flowPyramid = (float **)malloc(levels * sizeof(float *));
-        for (int k = levels - 1; k >= 0; k--)
-        {
-            int tmp_h = src.rows >> k;
-            int tmp_w = src.cols >> k;
-            flowPyramid[k] = (float *)malloc(tmp_h * tmp_w * 2 * sizeof(float));
-            gpu::calc_opt_flow(prevPyramid[k], pyramid[k], tmp_w, tmp_h, flowPyramid, k, levels);
-            //cpu::calc_optical_flow(prevPyramid[k], pyramid[k], tmp_w, tmp_h, flowPyramid, k, levels);
-        }
+				for(int k = levels - 1; k >= 0; k--)
+				{
+					int tmp_h = src.rows >> k;
+					int tmp_w = src.cols >> k;
+					gpu::calc_opt_flow(prev_pyramid[k], pyramid[k], tmp_w, tmp_h, flow_pyramid, k, levels);
+					//cpu::calc_optical_flow(prev_pyramid[k], pyramid[k], tmp_w, tmp_h, flow_pyramid, k, levels);
+				}
 
         // Visualize flow field
-        int level = 0;
-        h = src.rows >> level;
-        w = src.cols >> level;
-        int offset = w / 30;
-        cv::Mat test(src.rows, src.cols, CV_8UC3);
-        utils::upscale_3ch(pyramid[level], w, h, level, test.data);
-        for (int i = 0; i < h; i += offset)
-        {
-            for (int j = 0; j < w; j += offset)
-            {
-                // For every point of prev
-                //int pos = i * w + j;
-                // Find cumulative flow of all previous levels
-                float u, v;
-                u = v = 0;
-                for (int k = levels - 1; k >= level; k--)
-                {
-                    int scale = k - level;
-                    int tmp_i = i >> scale;
-                    int tmp_j = j >> scale;
-                    int tmp_pos = tmp_i * (w >> scale) + tmp_j;
-                    int multiplier = 1 << scale;
-                    u += (double)multiplier * flowPyramid[k][tmp_pos * 2];
-                    v += (double)multiplier * flowPyramid[k][tmp_pos * 2 + 1];
-                }
-                
-                //Ova e staveno za skaliranje na strelkite za podobra vizuelizacija
-                if(u > offset){
-                    u = offset;
-                }else if(u < - offset){
-                    u = - offset;
-                }
-                if(v > offset){
-                    v = offset;
-                }else if(v < - offset){
-                    v = - offset;
-                }
-                int tmp_ni = (int)(v + i);
-                int tmp_nj = (int)(u + j);
+				int level = 0;
+				int arrowRes = 30;
+				visualizeFlowField(src, pyramid, flow_pyramid, levels, level, arrowRes);
 
-                //Ova e za da se izvadat nekoi problematichni presmetki
-                if(tmp_ni < 0 || tmp_nj < 0){
-                    continue;
-                }
+				// Assign the current flow pyramid to the previous one
+				unsigned char** swap = prev_pyramid;
+				prev_pyramid = pyramid;
+				pyramid = swap;
 
-                cv::arrowedLine(test, cv::Point(j, i), cv::Point(tmp_nj, tmp_ni), cv::Scalar(0, 0, 255), 1, 8, 0, 0.4);
-            }
-        }
-        cv::imshow("Optical Flow Field", test);
-
-        // Free malloc
-				// index 0 is src
-				if(prevPyramidAlloc){
-					free(prevPyramid[0]);
-					prevPyramidAlloc = false;
-				}
-        for (int i = 1; i < levels; i++)
-        {
-            free(prevPyramid[i]);
-        }
-        free(prevPyramid);
-        prevPyramid = pyramid;
-        for (int i = 1; i < levels; i++)
-        {
-            free(flowPyramid[i]);
-        }
-        free(flowPyramid);
-
-        if (cv::waitKey(5) == 27)
-        {
-            return 0;
-        }
+        if (cv::waitKey(5) == 27) return 0;
     }
+
+		free_pyramid(&prev_pyramid, levels);
+		free_pyramid(&pyramid, levels);
+		free_pyramid(&flow_pyramid, levels);
 
     return 0;
 }
